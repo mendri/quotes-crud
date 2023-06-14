4a01bfb325c351773f2bde85830cf75026003475
mendri <marcalmendrighi@gmail.com>
2023-06-14 18:38:02 -0300

    feat: protótipo de tela de registro

---
+++ frontend/src/Interfaces/ISubmitHeaderBlogCRUD.ts
@@ -0,0 +1,6 @@
+interface SubmitHeaderBlogCRUD {
+  main_color: string
+  sub_color: string
+}
+
+export default SubmitHeaderBlogCRUD
--- frontend/src/Pages/Register.tsx
+++ frontend/src/Pages/Register.tsx
@@ -1,17 +1,24 @@
 import { useNavigate } from "react-router-dom"
 import SubmitHeaderBlogCRUD from "../Components/SubmitHeaderBlogCRUD"
 
-function Login() {
+function Register() {
   const navigate = useNavigate()
 
   return (
     <main className="w-full h-full flex">
-      <SubmitHeaderBlogCRUD main_color="indigo" sub_color="cyan" />
+      <SubmitHeaderBlogCRUD main_color="orange" sub_color="yellow" />
       <section className="w-2/3 flex flex-col justify-center items-center bg-gradient-to-b from-slate-300 via-slate-50 to-slate-300">
         <form className="mt-10 w-2/3 h-2/3 flex flex-col justify-end items-center" onSubmit={ (e) => e.preventDefault() }>
           <p className="text-2xl font-extrabold w-3/5 text-center mb-14">
-            Olá! Digite seu email e senha para acessar o Blog-CRUD.
+            Olá! Digite um nome de usuário e um email que não esteja registrado e uma senha segura para criar sua conta no 
+            <br />
+            Blog-CRUD.
           </p>
+          <div className="flex flex-col items-center mb-4">
+            <label htmlFor="username-input">
+              <input placeholder="Nome de usuário" id="username-input" type="text" className="mt-2 p-1 border-2 border-gray-400 shadow-2xl"/>
+            </label>
+          </div>
           <div className="flex flex-col items-center mb-4">
             <label htmlFor="email-input">
               <input placeholder="Email" id="email-input" type="email" className="mt-2 p-1 border-2 border-gray-400 shadow-2xl"/>
@@ -23,11 +30,11 @@ function Login() {
             </label>
           </div>
           <div className="w-full flex flex-col items-center">
-            <button className="mb-2 text-white text-xl font-bold w-4/12 h-10 rounded-lg bg-gradient-to-tr from-cyan-300 to-cyan-500 shadow-2xl">
-              Logar
-            </button>
-            <button className="mb-2 text-white text-xl font-bold w-4/12 h-10 rounded-lg bg-gradient-to-tr from-orange-300 to-orange-500 shadow-2xl" onClick={() => navigate("/register")}>
+            <button className="mb-2 text-white text-xl font-bold w-4/12 h-10 rounded-lg bg-gradient-to-tr from-orange-300 to-orange-500 shadow-2xl">
               Criar Conta
+            </button>
+            <button className="mb-2 text-white text-xl font-bold w-4/12 h-10 rounded-lg bg-gradient-to-tr from-cyan-300 to-cyan-500 shadow-2xl" onClick={() => navigate("/login")}>
+              Logar
             </button>
           </div>
         </form>
@@ -36,4 +43,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Register
